c13b09fbc792e48ad21690a94fbac640fa41bfce
roarsher <154459620+roarsher@users.noreply.github.com>
2026-04-02 09:14:57 +0530

    change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smartcollegemanagementsystem.onrender.com",
+  baseURL: "https://smartcollegemanagementsystem.onrender.com/api",
 });
 
 // Attach JWT automatically
